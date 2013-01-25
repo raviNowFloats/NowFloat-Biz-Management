625dc717bae1fb2c1d0bfb21c7b4c96b95b2fce5
Sumanta Roy <sumanta.roy@nowfloats.com>
2013-01-25 12:28:19 +0530

    Initial Commit

---
+++ NowFloat Biz Management/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NowFloat Biz Management
+//
+//  Created by Sumanta Roy on 25/01/13.
+//  Copyright (c) 2013 NowFloats Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NowFloat Biz Management/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NowFloat Biz Management
+//
+//  Created by Sumanta Roy on 25/01/13.
+//  Copyright (c) 2013 NowFloats Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
