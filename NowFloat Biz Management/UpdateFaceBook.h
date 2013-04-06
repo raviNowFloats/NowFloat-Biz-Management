a0abca2b57ae9de09dfeae79faff3a149c100d11
Sumanta Roy <sumanta.roy@nowfloats.com>
2013-04-06 15:50:41 +0530

    Image Uploads , Facebook posting upload

---
+++ NowFloat Biz Management/UpdateFaceBook.h
@@ -0,0 +1,13 @@
+//
+//  UpdateFaceBook.h
+//  NowFloats Biz Management
+//
+//  Created by Sumanta Roy on 12/03/13.
+//  Copyright (c) 2013 NowFloats Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UpdateFaceBook : NSObject
+
+@end
---
+++ NowFloat Biz Management/UpdateFaceBook.m
@@ -0,0 +1,13 @@
+//
+//  UpdateFaceBook.m
+//  NowFloats Biz Management
+//
+//  Created by Sumanta Roy on 12/03/13.
+//  Copyright (c) 2013 NowFloats Technologies. All rights reserved.
+//
+
+#import "UpdateFaceBook.h"
+
+@implementation UpdateFaceBook
+
+@end
